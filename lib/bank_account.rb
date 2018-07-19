dfc46c7f79b5eee8e663a934e2c7546e1243f12b
github username <github email address>
2018-07-19 16:06:13 -0500

    Completed assignment with Becci

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class BankAccount
+
+  attr_reader :name, :display_balance
+  attr_accessor :status, :balance
+
+  def initialize(name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
+
+  def deposit(money)
+    self.balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if self.status == "open" && self.balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
 class Transfer
   # your code here
+  # acts as a space for a transaction between two instances of the bank account class.
+  #you can't just transfer money to another account without the bank running checks first
+  #check the validity of the accounts before the transaction occurs
+  #should be able to reject a transfer if the accounts aren't valid or if the sender doesn't have the money
+
+  #Transfers start out in a "pending" status. They can be executed and go to a "complete" state. They can also go to a "rejected" status. A completed transfer can also be reversed and go into a "reversed" status.
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+      if !self.valid?
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      elsif @sender.balance > self.amount && @status == "pending"
+        @sender.balance -= @amount
+        @receiver.deposit(@amount)
+        @status = "complete"
+      else
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @status = "reversed"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+    end
+  end
 end
